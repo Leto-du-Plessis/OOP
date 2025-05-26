a3e5ae67630453ea788ecd600cd8efbb3f87dee3
Leto du Plessis <letoduplessis@gmail.com>
2025-05-26 08:49:11 +0200

    Finished initial example.

--- functions.py
+++ functions.py
@@ -49,7 +49,7 @@ print(" and ".join(f"{user["username"]} likes {user["favourite_movie"]}" for use
 
 # For the sake of pedagogy it's worth noting that it's generally going to be better to store users in a dictionary where the username is given as the key.
 # This provides a significant lookup time speedup. O(N) -> O(1). But this is besides the point for now. 
-# The structure we have now is actually quite a good way to store the information we want to store. 
+# The structure we have now is actually quite a good way to store the information we want to store, as the information is now. 
 # Let's say now that we want to check whether two users like the same movie. We could define a function to do this.
 
 def compare_movie(movie1: str, movie2: str) -> bool:
@@ -59,3 +59,25 @@ def compare_movie(movie1: str, movie2: str) -> bool:
 
 print(compare_movie(users[0]["favourite_movie"], users[1]["favourite_movie"]))
 print(compare_movie(users[0]["favourite_movie"], "Across the Spiderverse"))
+
+# Now, we've introduced some level of nuisance into our workflow. Firstly, say we propagate our users list into a difference context. We need to explicitely
+# bring our new function with us. Or else, we'd need to redefine compare_movie(). Now, in this case that's hardly a hardship, but consider how this might be 
+# painful if the relevant function were more complex. 
+# A second point is that, note we got a "True" boolean when we provided a string as an argument to compare against the first users favourite movie. This was the 
+# intended behaviour in this case, but note that we may not want this behaviour since if we can provide an arbitrary string as an argument, that string could represent
+# something other than a movie. 
+# In other words, our function has no way to determine whether or not the provided strings are associated with the favourite_movie of a user. 
+# We also have no real way to compare users. Say we add another user:
+
+users.append(create_user("Leto", "ThisPasswordIsTotallySecure", "Across the Spiderverse"))
+
+# Is this user the "same" as user 0? 
+
+print(users[0] == users[3])
+
+# This above comparison is making use of a dictionary equality method. Clearly, it says, yes, they are the same! 
+# This might be what we wanted. But, what if this is a different user who just happens to have the same name, password and favourite movie? 
+# The point is, we may want more control over this behaviour. 
+# We could define a function which compares two users explicitely. However, we now have another function to carry around with us. 
+# Furthermore, we would then lose the functionality of the == operator. There is a way to maintain this functionality ... 
+
