3f57ffebcc0bb96bbb430e14fc00fdc4ee28f32e
Kaimons <kairan.moorlach@gmail.com>
2025-05-30 13:49:12 +0200

    update 2

---
+++ Market.py
@@ -0,0 +1,61 @@
+from Worker_classes import Worker
+from Worker_type import Farmer, Lumberjack, Blacksmith
+import numpy as np
+
+class Market:
+    def __init__(self, wood_stock, food_stock, weapon_stock):
+        self.wood_stock = wood_stock
+        self.food_stock = food_stock
+        self.weapon_stock = weapon_stock
+
+    array = [[[]for i in range(2)] for i in range (3)]
+
+    def sale(self):
+        if Worker.self.weapons > 10:
+            x = Worker.self.weapons - 10
+            self.array[2][0].append(x)
+    def price_weapons(self):
+        count = len(self.array[2])
+        price=0
+        if count > 100:
+            price == 1
+
+        elif  75 < count <= 100:
+            price == 2
+
+        elif 50 < count <= 75:
+            price == 3
+        
+        elif 25 < count <= 50:
+            price == 4
+
+        else:
+            price ==5 
+
+        self.array[2][1] .append(price)  
+
+    def market(self):
+        Market.sale(self)
+        Market.price_weapons(self)
+        print(self.array)
+
+
+
+man1 = Blacksmith(2, 10, 10, 15)
+
+
+
+x=0
+while x != 100 or man1.alive() == False:
+    man1.action()
+    
+    if man1.alive() == True:
+        man1.hunger()
+        man1.eat()
+    else:
+        break    
+    print(f'wood total', man1.wood)
+    print(f'food total', man1.food)
+    print(f'weapon total',man1.weapons)
+    print(f'day',x, 'over')
+    x = x + 1
--- Worker_classes.py
+++ Worker_classes.py
@@ -1,21 +1,60 @@
+import numpy as np
+
 class Worker:
-    def __init__(self, eat, int):
-        self.eat = eat
+    hunger_level = 100
+    maximum = 80
+    minimum = 30
+    def __init__(self, food, wood, gold, weapons):
+        self.food = food
+        self.wood = wood
+        self.gold = gold
+        self.weapons = weapons
+
+    def hunger(self):
+        self.hunger_level = self.hunger_level - np.random.randint(20,31)
+        print(f'The hunger before eating', self.hunger_level)
+
+    def eat(self):
+        will=np.random.randint(0,10)
+        print(f'The will to eat', will)
+        if self.hunger_level <= self.minimum and self.food > 0:
+            if self.food > 0 and self.wood > 0:
+                self.food = self.food -1
+                self.wood = self.wood -1
+                self.hunger_level = self.hunger_level + np.random.randint(45,55)                    
+            else:
+                self.food = self.food-1
+                self.hunger_level = self.hunger_level + np.random.randint(10,21)
+
+        if will >= 3:
+
+            if self.minimum < self.hunger_level < self.maximum:
+                if self.food >=0:
+                    if self.wood >=2 and self.hunger_level < 50:
+                        self.wood = self.wood - 1
+                        self.food = self.food - 1
+                        self.hunger_level = self.hunger_level + np.random.randint(45,55)
+                
+                    elif self.hunger_level > 50:
+                        self.food = self.food - 1
+                        self.hunger_level = self.hunger_level + np.random.randint(10,21)
+                
+                
+                    else:
+                        self.food = self.food-1
+                        self.hunger_level = self.hunger_level + np.random.randint(10,21)
 
 
-class Framer(Worker):
-    def __init__(self, eat: int, produce_food: int):
-        super().__init__(eat)
-        self.produce_food = produce_food
 
-class Lumberjack(Worker):
-    def __init__(self, eat: int, produce_wood: int):
-        super().__init__(eat)
-        self.produce_wood = produce_wood
+        print(f'the hunger level after eating:', self.hunger_level)
 
-class Blacksmith(Worker):
-    def __init__(self, eat: int, use_resource: int, produce_weapon: int):
-        super().__init__(eat)
-        self.use_resource = use_resource
-        self.produce_weapon = produce_weapon
-        
+    def alive(self):
+        alive = True
+
+
+        if self.hunger_level <= 0:
+            alive = False  
+        return alive
+
+
+
